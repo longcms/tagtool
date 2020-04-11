13a264d3a8946575d42bccd28a95803d1a562ce6
longcms <361604181@qq.com>
2020-04-11 14:57:57 +0800

    增加抗锯齿
    放大效果能显示像素化

--- src/components/tagtool/js/ShapeCanvas.js
+++ src/components/tagtool/js/ShapeCanvas.js
@@ -1200,6 +1200,10 @@ function ShapeCanvas () {
       this.x = _x
       this.y = _y
       d.clearRect(0, 0, this.graph.width, this.graph.height)
+      d.mozImageSmoothingEnabled = false
+      d.webkitImageSmoothingEnabled = false
+      d.msImageSmoothingEnabled = false
+      d.imageSmoothingEnabled = false
       d.drawImage(A, _mx, _my, _mw, _mh, 0, 0, _w, _h)
       this.graph.style.display = ''
       this.dataChange()
